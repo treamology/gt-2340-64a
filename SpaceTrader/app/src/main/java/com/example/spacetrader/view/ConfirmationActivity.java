f06d1c2363980ef215fbaa1841b54d0b9f5f9096
Eric Lee <elee351@gatech.edu>
2019-02-18 06:25:47 -0500

    Confirmation Dialog Box

--- SpaceTrader/app/src/main/java/com/example/spacetrader/view/ConfirmationActivity.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/view/ConfirmationActivity.java
@@ -1,4 +1,38 @@
 package com.example.spacetrader.view;
 
-public class ConfirmationActivity {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import com.example.spacetrader.model.GameState;
+import com.example.spacetrader.model.Player;
+import com.example.spacetrader.viewmodel.ConfigurationViewModel;
+
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class ConfirmationActivity extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(true);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Continue?");
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        return builder.create();
+    }
+
+    //onCreate method
+    //clicking button -> call method that opens dialog box ->  create instance of ConfirmationActivity
+    // -> .show()
 }
