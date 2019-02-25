4f2a175bb6695670630eebb87af901723e365616
Donny Lawrence <trea@treamous.com>
2019-02-24 20:01:59 -0500

    Move system generation stuff into the Universe class itself.

--- SpaceTrader/app/src/main/java/com/example/spacetrader/model/Universe.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/model/Universe.java
@@ -3,6 +3,9 @@ package com.example.spacetrader.model;
 import com.example.spacetrader.model.system.Planet;
 import com.example.spacetrader.model.system.PlanetGeneration;
 import com.example.spacetrader.model.system.Position;
+import com.example.spacetrader.model.system.ResourceBias;
+import com.example.spacetrader.model.system.SolarSystem;
+import com.example.spacetrader.model.system.TechLevel;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -13,30 +16,66 @@ import java.util.Random;
  */
 public class Universe {
 
-    private static ArrayList<Planet> planets;
+    private static ArrayList<SolarSystem> systems;
     private static final int sizeX = 150; //sizeX and sizeY are the maximum coordinates of the map
     private static final int sizeY = 100;
     private static HashSet<Position> takenPositions;
+    private static HashSet<Integer> takenNames;
 
-    public static void createUniverse () {
-        PlanetGeneration.clearNames();
+    public static void createUniverse() {
         takenPositions = new HashSet<>();
-        planets = new ArrayList<>();
-        for (int i = 0; i < Planet.getNumPlanets(); i++) {
-            Planet current = new Planet();
-            planets.add(current);
+        takenNames = new HashSet<>();
+        systems = new ArrayList<>();
+        for (int i = 0; i < SolarSystem.MAX_SYSTEMS; i++) {
+            SolarSystem current = generateSystem();
+            systems.add(current);
         }
     }
 
-    public static Position generatePosition() {
+    /**
+     * Generates a single solar system.
+     *
+     * @return A new, randomly generated solar system.
+     */
+    private static SolarSystem generateSystem() {
+        TechLevel techLevel = TechLevel.values()[GameState.rng.nextInt(TechLevel.values().length)];
+        ResourceBias resourceBias = ResourceBias.values()[GameState.rng.nextInt(ResourceBias.values().length)];
+        Position position = generateSystemPosition();
+        String name = generateSystemName();
+        return new SolarSystem(name, position, techLevel, resourceBias);
+    }
+
+    public static Position generateSystemPosition() {
         int x = GameState.rng.nextInt(sizeX);
         int y = GameState.rng.nextInt(sizeY);
         Position pos = new Position(x, y);
         if (takenPositions.contains(pos)) {
-            return generatePosition();
+            return generateSystemPosition();
         } else {
             takenPositions.add(pos);
             return pos;
         }
+    }
+
+    /**
+     * Randomly picks a name from the planet without allowing duplicates
+     * @return the name
+     */
+    public static String generateSystemName() {
+        String name = null;
+        int index = GameState.rng.nextInt(SolarSystem.NAMES.length);
+        int endLoop = index;
+        do {
+            if (!takenNames.contains(index)) {
+                takenNames.add(index);
+                name = SolarSystem.NAMES[index];
+            } else {
+                index = (index + 1) % SolarSystem.NAMES.length;
+            }
+        } while (name == null && index != endLoop);
+        if (name == null) {
+            name = "Unidentified planet";
+        }
+        return name;
     }
 }
--- SpaceTrader/app/src/main/java/com/example/spacetrader/model/system/SolarSystem.java
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/model/system/SolarSystem.java
@@ -3,18 +3,40 @@ package com.example.spacetrader.model.system;
 import com.example.spacetrader.model.GameState;
 import com.example.spacetrader.model.Universe;
 
-public abstract class SolarSystem {
+import java.util.HashSet;
+import java.util.Random;
+
+public class SolarSystem {
+
+    public static final int MAX_SYSTEMS = 10;
+    public static final String[] NAMES = new String[]{"Acamar", "Adahn", "Aldea", "Campor", "Deneb"
+            , "Endor", "Hulst", "Janus", "Ligon", "Melina", "Relva", "Sol", "Antedi", "Balonsee"
+            , "Baratas", "Brax", "Bretel", "Calondia", "Capelle", "Carzon", "Castor", "Cestus"
+            , "Cheron", "Courteny", "Dale", "Damast", "Davlos", "Deneva", "Devidia"
+            , "Draylon", "Drema", "Esmee", "Exo", "Ferris", "Festen", "Fourmi", "Frolix", "Gemulon"
+            , "Guinifer", "Hades", "Hamlet", "Helena", "Iodine", "Iralius", "Japori", "Jarada"
+            , "Jason", "Kaylon", "Khefka", "Kira", "Klaatu", "Klaestron", "Korma", "Kravat", "Krios"
+            , "Laertes", "Largo", "Lave", "Lowry", "Magrat", "Malcoria", "Mentar", "Merik"
+            , "Mintaka", "Montor", "Mordan", "Myrthe", "Nelvana", "Nix", "Nyle", "Odet", "Og"
+            , "Omega", "Omphalos", "Oria", "Othello", "Parade", "Penthara", "Picard", "Pollux"
+            , "Quator", "Rakhar", "Ran", "Regulus", "Rhymus", "Rochani", "Rubicum", "Rutia"
+            , "Sarpeidon", "Sefalla", "Seltrice", "Sigma", "Somari", "Stakoron", "Styris", "Telani"
+            , "Tamus", "Tantalos", "Tarnuga", "Tarkhannon", "Terosa", "Thera", "Titan", "Torin"
+            , "Triacus", "Turkana", "Tyrus", "Umberlee", "Utopia", "Vadera", "Vagra", "Vandor"
+            , "Ventax", "Xenon", "Xerxes", "Yew", "Yojimbo", "Zalkon", "Zuul"};
+
+
 
     String name;
     Position position;
     TechLevel techLevel;
     ResourceBias resourceBias;
 
-    public SolarSystem(String name, int x, int y) {
+    public SolarSystem(String name, Position position, TechLevel techLevel, ResourceBias resourceBias) {
         this.name = name;
-        techLevel = TechLevel.values()[GameState.rng.nextInt(TechLevel.values().length)];
-        resourceBias = ResourceBias.values()[GameState.rng.nextInt(ResourceBias.values().length)];
-        position = Universe.generatePosition();
+        this.position = position;
+        this.techLevel = techLevel;
+        this.resourceBias = resourceBias;
     }
 
     public TechLevel getTechLevel() {
