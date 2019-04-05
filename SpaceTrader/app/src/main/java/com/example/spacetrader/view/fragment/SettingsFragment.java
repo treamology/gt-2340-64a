7967aeeeb3cab43dc377c1b2e7cc68833b99dec3
Eric Lee <ericshlee97@gmail.com>
2019-04-04 21:39:36 -0400

    Buttons in Fragment; Represent Persistence with Save/Load Buttons

---
+++ SpaceTrader/app/src/main/java/com/example/spacetrader/view/fragment/SettingsFragment.java
@@ -0,0 +1,16 @@
+package com.example.spacetrader.view.fragment;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+public class SaveFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Button button = (Button) findViewById(
+    }
+}
